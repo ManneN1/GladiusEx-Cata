638449de3e9a64c0bfe574fa8123e725c86d02de
ManneN1 <ManneN1@users.noreply.github.com>
2024-04-25 22:19:44 +0200

    Tried fixing DK spec detection
    I'm not that into DK, but I hope this is at least more correct.

--- Data.lua
+++ Data.lua
@@ -2,7 +2,7 @@ local GladiusEx = _G.GladiusEx
 local L = LibStub("AceLocale-3.0"):GetLocale("GladiusEx")
 
 function GladiusEx:GetSpecList()
-    return {      
+    return {
         -- WARRIOR
         [GladiusEx:SafeGetSpellName(12294)] = 71,       -- Mortal Strike
         [GladiusEx:SafeGetSpellName(46924)] = 71,       -- Bladestorm
@@ -43,34 +43,36 @@ function GladiusEx:GetSpecList()
         [GladiusEx:SafeGetSpellName(14751)] = 257,      -- Chakra
         [GladiusEx:SafeGetSpellName(47788)] = 257,      -- Guardian Spirit
         [GladiusEx:SafeGetSpellName(15487)] = 258,      -- Silence
-        [GladiusEx:SafeGetSpellName(34914)] = 258,      -- Vampiric Touch   
-        [GladiusEx:SafeGetSpellName(15407)] = 258,      -- Mind Flay        
+        [GladiusEx:SafeGetSpellName(34914)] = 258,      -- Vampiric Touch
+        [GladiusEx:SafeGetSpellName(15407)] = 258,      -- Mind Flay
         [GladiusEx:SafeGetSpellName(15473)] = 258,      -- Shadowform
         [GladiusEx:SafeGetSpellName(15286)] = 258,      -- Vampiric Embrace
-        -- DEATHKNIGHT                                  
+        -- DEATHKNIGHT
         [GladiusEx:SafeGetSpellName(55050)] = 250,      -- Heart Strike
-        --[GladiusEx:SafeGetSpellName(49016)] = 250,      -- Hysteria
+        [GladiusEx:SafeGetSpellName(49222)] = 250,      -- Bone Shield
         [GladiusEx:SafeGetSpellName(53138)] = 250,      -- Abomination's Might
         [GladiusEx:SafeGetSpellName(49203)] = 251,      -- Hungering Cold
         [GladiusEx:SafeGetSpellName(49143)] = 251,      -- Frost Strike
         [GladiusEx:SafeGetSpellName(49184)] = 251,      -- Howling Blast
         [GladiusEx:SafeGetSpellName(55610)] = 251,      -- Imp. Icy Talons
         [GladiusEx:SafeGetSpellName(55090)] = 252,      -- Scourge Strike
-        [GladiusEx:SafeGetSpellName(49222)] = 252,      -- Bone Shield
         [GladiusEx:SafeGetSpellName(49206)] = 252,      -- Summon Gargoyle
         [GladiusEx:SafeGetSpellName(51052)] = 252,      -- Anti-Magic Zone
-        [GladiusEx:SafeGetSpellName(49194)] = 252,      -- Unholy Blight    
-        -- MAGE                                         
+        [GladiusEx:SafeGetSpellName(49194)] = 252,      -- Unholy Blight
+        [GladiusEx:SafeGetSpellName(49016)] = 252,      -- Unholy Frenzy
+
+        -- MAGE
         [GladiusEx:SafeGetSpellName(44425)] = 62,       -- Arcane Barrage
         [GladiusEx:SafeGetSpellName(31583)] = 62,       -- Arcane Empowerment
+        [GladiusEx:SafeGetSpellName(31589)] = 62,       -- Slow
         [GladiusEx:SafeGetSpellName(44457)] = 63,       -- Living Bomb
         [GladiusEx:SafeGetSpellName(31661)] = 63,       -- Dragon's Breath
         [GladiusEx:SafeGetSpellName(11366)] = 63,       -- Pyroblast
-        [GladiusEx:SafeGetSpellName(11129)] = 63,       -- Combustion       
+        [GladiusEx:SafeGetSpellName(11129)] = 63,       -- Combustion
         [GladiusEx:SafeGetSpellName(44572)] = 64,       -- Deep Freeze
         [GladiusEx:SafeGetSpellName(31687)] = 64,       -- Summon Water Elemental
-        [GladiusEx:SafeGetSpellName(11426)] = 64,       -- Ice Barrier      
-        -- WARLOCK                                     
+        [GladiusEx:SafeGetSpellName(11426)] = 64,       -- Ice Barrier
+        -- WARLOCK
         [GladiusEx:SafeGetSpellName(48181)] = 265,      -- Haunt
         [GladiusEx:SafeGetSpellName(30108)] = 265,      -- Unstable Affliction
         [GladiusEx:SafeGetSpellName(59672)] = 266,      -- Metamorphosis
@@ -78,7 +80,7 @@ function GladiusEx:GetSpecList()
         [GladiusEx:SafeGetSpellName(30283)] = 267,      -- Shadowfury
         [GladiusEx:SafeGetSpellName(30299)] = 267,      -- Nether Protection
         [GladiusEx:SafeGetSpellName(17962)] = 267,      -- Conflagrate
-        -- SHAMAN                                       
+        -- SHAMAN
         [GladiusEx:SafeGetSpellName(51490)] = 262,      -- Thunderstorm
         [GladiusEx:SafeGetSpellName(16166)] = 262,      -- Elemental Mastery
         [GladiusEx:SafeGetSpellName(51470)] = 262,      -- Elemental Oath
@@ -89,7 +91,7 @@ function GladiusEx:GetSpecList()
         [GladiusEx:SafeGetSpellName(60103)] = 263,      -- Lava Lash
         [GladiusEx:SafeGetSpellName(61295)] = 264,      -- Riptide
         [GladiusEx:SafeGetSpellName(51886)] = 264,      -- Cleanse Spirit
-        [GladiusEx:SafeGetSpellName(974)]   = 264,      -- Earth Shield     
+        [GladiusEx:SafeGetSpellName(974)]   = 264,      -- Earth Shield
         -- HUNTER
         [GladiusEx:SafeGetSpellName(19577)] = 253,      -- Intimidation
         [GladiusEx:SafeGetSpellName(20895)] = 253,      -- Spirit Bond
@@ -100,7 +102,7 @@ function GladiusEx:GetSpecList()
         [GladiusEx:SafeGetSpellName(19434)] = 254,      -- Aimed Shot
         [GladiusEx:SafeGetSpellName(53301)] = 255,      -- Explosive Shot
         [GladiusEx:SafeGetSpellName(19386)] = 255,      -- Wyvern Sting
-        
+
         -- DRUID
         [GladiusEx:SafeGetSpellName(48505)] = 102,      -- Starfall
         [GladiusEx:SafeGetSpellName(50516)] = 102,      -- Typhoon
@@ -110,8 +112,8 @@ function GladiusEx:GetSpecList()
         [GladiusEx:SafeGetSpellName(33878)] = 103,      -- Mangle (Bear)
         [GladiusEx:SafeGetSpellName(24932)] = 103,      -- Leader of the Pack
         [GladiusEx:SafeGetSpellName(18562)] = 105,      -- Swiftmend
-        [GladiusEx:SafeGetSpellName(48438)] = 105,      -- Wild Growth      
-        [GladiusEx:SafeGetSpellName(33891)] = 105,      -- Tree of Life     
+        [GladiusEx:SafeGetSpellName(48438)] = 105,      -- Wild Growth
+        [GladiusEx:SafeGetSpellName(33891)] = 105,      -- Tree of Life
     }
 end
 
@@ -200,6 +202,7 @@ function GladiusEx:GetDefaultImportantAuras()
         [GladiusEx:SafeGetSpellName(19574)] = 8.9, -- Bestial Wrath
         [GladiusEx:SafeGetSpellName(34692)] = 8.9, -- The Beast Within
         [GladiusEx:SafeGetSpellName(6940)] = 8.8, -- Hand of Sacrifice
+        [GladiusEx:SafeGetSpellName(49016)] = 8.8, -- Unholy Frenzy
         [GladiusEx:SafeGetSpellName(62618)] = 8.7, -- Strength of Soul
         [GladiusEx:SafeGetSpellName(23920)] = 8.7, -- Spell Reflection
         [GladiusEx:SafeGetSpellName(8178)] = 8.7, -- Grounding Totem Effect
@@ -269,7 +272,6 @@ function GladiusEx:GetDefaultImportantAuras()
         [GladiusEx:SafeGetSpellName(31884)] = 2.5, -- Avenging Wrath
         [GladiusEx:SafeGetSpellName(33206)] = 2.5, -- Pain Suppression
         [GladiusEx:SafeGetSpellName(498)] = 2.5, -- Divine Protection
-        [GladiusEx:SafeGetSpellName(49016)] = 2.5, -- Unholy Frenzy
         [GladiusEx:SafeGetSpellName(12328)] = 2.4, -- Sweeping Strikes
         [GladiusEx:SafeGetSpellName(13877)] = 2.4, -- Blade Flurry
         [GladiusEx:SafeGetSpellName(22842)] = 2.1, -- Frenzied Regeneration
@@ -460,9 +462,9 @@ GladiusEx.specIDToClassID = {
 
 GladiusEx.classNamesByID = {
     [1] =  "WARRIOR",
-    [2] =  "PALADIN", 
+    [2] =  "PALADIN",
     [3] =  "HUNTER",
-    [4] =  "ROGUE", 
+    [4] =  "ROGUE",
     [5] =  "PRIEST",
     [6] =  "DEATHKNIGHT",
     [7] =  "SHAMAN",
@@ -472,7 +474,7 @@ GladiusEx.classNamesByID = {
 }
 
 GladiusEx.auraTypesByID = {
-    [0] = "None", 
+    [0] = "None",
     [1] = "Magic",
     [2] = "Curse",
     [3] = "Disease",
